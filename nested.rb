91683dedbbd995f90f8d5a6103ba00099f26b957
Victoria Hale <vic.hale74@gmail.com>
2019-09-26 01:58:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,7 @@ def changing_alan
     alans_new_info = "GUI"
 
     programmer_hash[:alan_kay][:known_for] = alans_new_info
-    return programmer_hash[:alan_kay]    
+    return programmer_hash[:alan_kay][:known_for]    
 end
 
 
