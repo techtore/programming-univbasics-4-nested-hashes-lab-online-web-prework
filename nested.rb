aca51fd274855846634d94a70eaf1ebace02f271
Victoria Hale <vic.hale74@gmail.com>
2019-09-26 01:56:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+ programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
@@ -64,7 +67,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+	programmer_hash =
+
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -79,7 +83,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash = 	{
+	:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+	}
     
 end
 
@@ -99,11 +108,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+    # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+    
+    alans_new_info = "GUI"
+
+    programmer_hash[:alan_kay][0] = alans_new_info
+    programmer_hash[:alan_kay][0]    
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
